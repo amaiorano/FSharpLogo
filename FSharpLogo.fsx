c599b0a3ccadda794d9c59610ff2b6ca411c6225
amaiorano <amaiorano@gmail.com>
2016-05-09 08:23:25 -0400

    Implement + and * operators on Vector2

--- FSharpLogo.fsx
+++ FSharpLogo.fsx
@@ -31,8 +31,8 @@ type Canvas(width, height) =
     let toGfxSpace (pt:Point) =
         Point(pt.X + width/2, height/2 - pt.Y)
 
-    member this.Width = float picBox.Width
-    member this.Height = float picBox.Height
+    member this.Width = float width
+    member this.Height = float height
     member this.DrawLine(pt1 :Point, pt2 :Point) =
         let pt1f = toGfxSpace pt1
         let pt2f = toGfxSpace pt2
@@ -47,6 +47,8 @@ type Vector2(x:float, y:float) =
     member this.X with get() = x and set (v) = x <- v
     member this.Y with get() = y and set (v) = y <- v
     member this.asPoint = Point(int(x + 0.5), int(y + 0.5))
+    static member (+) (v1:Vector2, v2:Vector2) = Vector2(v1.X + v2.X, v1.Y + v2.Y)
+    static member (*) (v1:Vector2, s) = Vector2(v1.X * s, v1.Y * s)
 
 
 let degToRad d = float(d) * System.Math.PI / 180.0
@@ -57,17 +59,15 @@ type Turtle(canvas:Canvas) =
     let mutable pos = Vector2()
     let mutable penDown = true
     let mutable angle = Math.PI/2.0;
-    
+    let facingVector () = Vector2(cos angle, sin angle)
     member this.PenUp = penDown <- false
     member this.PenDown = penDown <- true
     member this.FollowCommand (command:Command) =
         match command with
         | Forward units ->
-            let pt2 = Vector2()
-            pt2.X <- pos.X + cos angle * (float)units
-            pt2.Y <- pos.Y + sin angle * (float)units
-            canvas.DrawLine(pos.asPoint, pt2.asPoint)
-            pos <- pt2
+            let targetPos = pos + facingVector() * (float)units
+            canvas.DrawLine(pos.asPoint, targetPos.asPoint)
+            pos <- targetPos
         | Right degrees ->
             angle <- angle + (degToRad degrees)
         | _ ->
